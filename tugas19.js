7b01c44bf1d89dfeb17e0f95b5ad414200227b53
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-24 01:23:46 +0700

    tugas 19 Muhammad Rizwan

---
+++ tugas19.js
@@ -0,0 +1,25 @@
+/* Tugas 19 Javascript */
+// Muhammad Rizwan
+
+function tugasSembilanBelas(){
+    var kata = "Semangat...";
+    console.log("Jawaban Soal Pertama :");
+    console.log(kata.repeat(5));
+
+    console.log("");
+    
+    var bintang = "*";
+    console.log("Jawaban Soal Kedua :")
+    console.log(bintang.repeat(5));
+    console.log(bintang.repeat(25));
+    console.log(bintang.repeat(50));
+    console.log(bintang.repeat(75));
+    console.log(bintang.repeat(100));
+    console.log(bintang.repeat(75));
+    console.log(bintang.repeat(50));
+    console.log(bintang.repeat(25));
+    console.log(bintang.repeat(5));
+    
+}
+
+tugasSembilanBelas();
